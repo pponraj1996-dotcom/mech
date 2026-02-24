dc5b11cf52ea50433f4ec331f5d56f25b583aaa0
pponraj1996-dotcom <p.ponraj1996@gmail.com>
2026-02-24 14:37:50 +0530

    created part2.py file

---
+++ part2.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum_result = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum_result)
